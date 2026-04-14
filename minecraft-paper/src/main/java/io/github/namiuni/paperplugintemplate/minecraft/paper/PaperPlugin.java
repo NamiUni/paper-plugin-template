962ad510b51f4fc9f69bb945e1e086b7d8e70334
Unitarou <atelierkisame@gmail.com>
2026-04-14 16:35:18 +0900

    Cleanup

--- minecraft-paper/src/main/java/io/github/namiuni/paperplugintemplate/minecraft/paper/PaperPlugin.java
+++ minecraft-paper/src/main/java/io/github/namiuni/paperplugintemplate/minecraft/paper/PaperPlugin.java
@@ -21,13 +21,14 @@ package io.github.namiuni.paperplugintemplate.minecraft.paper;
 
 import io.github.namiuni.paperplugintemplate.common.infrastructure.configuration.configurations.PrimaryConfiguration;
 import io.github.namiuni.paperplugintemplate.common.infrastructure.persistence.UserRepository;
-import io.github.namiuni.paperplugintemplate.minecraft.paper.listeners.UserSessionHandler;
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
+import java.util.Set;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.CustomChart;
 import org.bstats.charts.SimplePie;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
 
@@ -37,19 +38,19 @@ public final class PaperPlugin extends JavaPlugin {
 
     private static final int BSTATS_PLUGIN_ID = 30597;
 
-    private final UserSessionHandler sessionHandler;
+    private final Set<Listener> listeners;
     private final UserRepository userRepository;
     private final ComponentLogger logger;
     private final Provider<PrimaryConfiguration> primaryConfig;
 
     @Inject
     private PaperPlugin(
-            final UserSessionHandler sessionHandler,
+            final Set<Listener> listeners,
             final UserRepository userRepository,
             final ComponentLogger logger,
             final Provider<PrimaryConfiguration> primaryConfig
     ) {
-        this.sessionHandler = sessionHandler;
+        this.listeners = listeners;
         this.userRepository = userRepository;
         this.logger = logger;
         this.primaryConfig = primaryConfig;
@@ -57,7 +58,7 @@ public final class PaperPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this.sessionHandler, this);
+        this.listeners.forEach(listener -> this.getServer().getPluginManager().registerEvents(listener, this));
 
         final Metrics metrics = new Metrics(this, BSTATS_PLUGIN_ID);
         final CustomChart chart = new SimplePie(
